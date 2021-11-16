ff62cb825817d7a829539b4f34c7eae2c26746db
AleksandrIs <aleksandris@wizardsdev.com>
2021-11-16 20:52:44 +0200

    Add project files.

---
+++ DIIA_UITest/POM/ServiceSwiperPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIIA_UITest.POM
+{
+    class ServiceSwiperPage
+    {
+        private readonly IWebDriver _webDriver;
+
+        private readonly By _card = By.XPath("//*[@id='gromadyanam']//*[@class = 'swiper_services-slide-title']/text()");
+
+        public ServiceSwiperPage(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public ServiceSwiperPage GoToServiceSwiperPage()
+        {
+            _webDriver.Navigate().GoToUrl("https://diia.gov.ua/");
+            return this;
+        }
+
+
+    }
+}
